76cbd492a5b2a3da261865e339729db90474758e
lha5268 <lhaveman5268@conestogac.on.ca>
2025-10-27 09:46:54 -0400

    updated workflow

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("surpriseBtn").addEventListener("click", () => {
-  const msg = document.getElementById("message");
-  msg.classList.toggle("hidden");
-  
-  if (!msg.classList.contains("hidden")) {
-    msg.style.animation = "fadeInUp 0.6s ease-out";
-  }
-});
